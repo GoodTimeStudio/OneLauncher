bf01bb3f92310465f923ca88829291d57f5084ee
Su Hao <1156407698@qq.com>
2015-09-05 23:05:12 +0800

    Added Download Page

---
+++ src/com/mcgoodtime/gtgames/gui/DownloadPanel.java
@@ -0,0 +1,18 @@
+package com.mcgoodtime.gtgames.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by suhao on 2015.9.5.0005.
+ *
+ * @author suhao
+ */
+public class DownloadPanel {
+
+    public DownloadPanel() {
+        JPanel containerDownload = new JPanel();
+        containerDownload.setBounds(2, 30, 696, 418);
+        containerDownload.setLayout(null);
+        MainWindow.mainPanel.add(containerDownload);
+    }
+}
